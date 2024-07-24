4f6d2bdd88378d2896e8800643302d7ec7ebabe6
William Bueno <will.b.bueno@gmail.com>
2024-07-24 12:51:03 -0300

    When
    Inclusão do when expression e statement.

---
+++ controle_fluxo_colecoes/when_expression.kt
@@ -0,0 +1,24 @@
+fun main (){
+
+    println(whenAssign("Hello"))
+    println(whenAssign(1))
+    println(whenAssign(3.4))
+    println(whenAssign(Myclass()))
+
+
+}
+
+fun whenAssign(obj: Any): Any {   // Atribuir quando os resultados forem compativeis. A diferença do when expression para o statement é que o express pode ser passado como atribuição de uma variável e não no corpo da função.
+
+    val result = when (obj) {     // Todos os valores passado pela chamada da função whenAssign serão verificados pela função when. 
+        is String -> obj.length   // A diferença entre os dois when é que o expression recebe os valores e faz uma atribuição a partir das condicionais. Já o statement verifica as condicionais e printa o resultado.
+        is Int -> obj + 10        // Funcionamento: Recebemos os valores a partir da chamada do whenAssign, recebemos o valor pelo parâmetro "obj" que verifica seu tipo, como temos uma tipagem any, pode ser qualquer valor.
+        is Double -> obj * 2      // A partir desta verificação, o when testa o parametro passado para cada linha de código condicional fornecida, se a checagem for positiva, ele para a condicional imediatamente e imprime este valor, caso contrario continuará a condicional até o término do código.
+        is Myclass -> "Esta é uma instância de Myclass"
+        else -> "Objeto desconhecido"
+
+        }
+        return result
+    
+}
+class Myclass
---
+++ controle_fluxo_colecoes/when_statement.kt
@@ -0,0 +1,34 @@
+/*
+
+When
+
+Pode ser usado no lugar de estrura de switch/case, porém de forma mais simples. 
+
+Quando você deseja executar um bloco de código condicionalmente, você pode usar a estrutura when.
+
+
+
+
+*/
+
+fun main() {
+    
+    cases("Hello")
+    cases(1)
+    cases(0L)
+    cases("hello")
+}
+
+fun cases(obj: Any) {
+    when (obj) {
+        1 -> println("One")
+        "Hello" -> println("Greeting")
+        is Long -> println("É do tipo Long")
+        !is String -> println("Não é uma string")
+        else -> println("Desconhecido")
+
+    }
+    
+}  
+
+
